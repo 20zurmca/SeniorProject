88976ab7b40708e648632ce6959e86ac48163123
zurmuhlc <zurmuhlc@lafayette.edu>
2020-03-02 21:49:05 -0500

    made a geocoding script that relies on google mapping api

---
+++ scripts/googlegeocoder.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+script that acquires longitude and latitude data for foreign schools
+"""
+import pandas
+import googlemaps
+
+df = pandas.read_csv("../data/International_High_Schools_Copy.csv")
+API_KEY = "" #your api key
+gmaps = googlemaps.Client(key=API_KEY)
+
+for i, row in df.iterrows():
+    address = None
+    if row['ADDRESS1']:
+        address = row['ADDRESS1']
+    else:
+        address = row['ADDRESS2']
+    
+    ls = [row['INSTITUTION'],
+          address,
+          row['CITY'],
+          row['PROVINCE'],
+          row['ZIP'],
+          row['COUNTRY']]
+    
+    addr = ' '.join(str(v) for v in ls)
+    result = gmaps.geocode(addr)
+    
+    if len(result) > 0:
+        df.at[i, 'LATITUDE'] = result[0]['geometry']['location']['lat']
+        df.at[i, 'LONGITUDE'] = result[0]['geometry']['location']['lng']
+        print("Added location for record ", i)
+        
+df.to_csv("../data/International_High_Schools_Copy.csv.csv")
