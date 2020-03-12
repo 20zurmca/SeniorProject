57a8cf561db1ee671a1b43bf60f7442067c0d77b
weix <weix@lafayette.edu>
2020-03-12 14:57:13 -0400

    roster master data

--- soccersite/map/models.py
+++ soccersite/map/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class RosterMasterData(models.Model):
+	rosterYear = models.IntegerField()
+	playerNumber = models.IntegerField()
+	firstName = models.CharField(maxlength = 50)
+	lastName = models.CharField(maxlength = 50)
+	year = models.CharField(maxlength = 10)
+	position1 = models.CharField(maxlength = 20)
+
+
+
+
