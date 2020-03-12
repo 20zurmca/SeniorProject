de3810fa245fb33afa325648e0d21203590c30e2
zurmuhlc <zurmuhlc@lafayette.edu>
2020-03-12 15:05:26 -0400

    added model for high school

--- soccersite/map/models.py
+++ soccersite/map/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class HighSchool(models.Model):
-    city           = models.CharField(max_length = 30)
-    instiution     = models.CharField(max_length = 100)
-    state_province = models.CharField(max_length = 20)
-    country        = models.CharField(max_length = 50)
-    latitude       = models.DecimalField(max_digits = 10)
-    longitude      = models.DecimalField(max_digits = 10)
-    type           = models.CharField(max_length = 20)
+    city            = models.CharField(max_length = 30)
+    instiution      = models.CharField(max_length = 100)
+    stateOrProvince = models.CharField(max_length = 20)
+    country         = models.CharField(max_length = 50)
+    latitude        = models.DecimalField(max_digits = 10)
+    longitude       = models.DecimalField(max_digits = 10)
+    schoolType      = models.CharField(max_length = 20)
