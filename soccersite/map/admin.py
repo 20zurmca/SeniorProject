ae605b55bc29abe4156f3157e75c458f1719c491
weix <weix@lafayette.edu>
2020-03-12 15:31:09 -0400

    changed admin to apply rostermasterdata and highschool model

--- soccersite/map/admin.py
+++ soccersite/map/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import RosterMasterData, HighSchool
 
 # Register your models here.
+admin.site.register(RosterMasterData)
+admin.site.register(HighSchool)
