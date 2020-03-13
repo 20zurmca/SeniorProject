a8b7a5f7f184a93fc07695deefc40470b996145b
zurmuhlc <zurmuhlc@lafayette.edu>
2020-03-13 01:51:07 -0400

    worked more on the front-end

---
+++ soccersite/map/static/map/map.js
@@ -0,0 +1,7 @@
+function initMap() {
+  var laf = {lat: 40.7000662,lng: -75.2103164}
+  var map = new google.maps.Map(
+    document.getElementByID('map'), {zoom: 4, center: laf})
+  );
+  var marker = new google.maps.Marker({position: easton, map: map});
+}
