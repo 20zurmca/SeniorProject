f20050bd54af134a265ffe3d19e644ee5c7279fd
zurmuhlc <zurmuhlc@lafayette.edu>
2020-03-13 01:15:16 -0400

    added HTML template

--- soccersite/map/views.py
+++ soccersite/map/views.py
@@ -6,4 +6,4 @@ GOOGLE_API_KEY = 'AIzaSyAH_r-WUtEbvF6_HAxLk9n5FF_6zJWImE4'
 
 def index(request):
     context = {'API_KEY': GOOGLE_API_KEY}
-    return render(request, 'polls/index.html', context)
+    return render(request, 'map/index.html', context)
