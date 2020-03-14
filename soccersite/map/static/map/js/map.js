1b3655d04586b3288ca6ae2822f690c48ee0b8d1
zurmuhlc <zurmuhlc@lafayette.edu>
2020-03-13 23:57:46 -0400

    added gesture handling to be greedy

--- soccersite/map/static/map/js/map.js
+++ soccersite/map/static/map/js/map.js
@@ -2,6 +2,7 @@ function initMap() {
   var map = new google.maps.Map(
       document.getElementById('map'), {
         center: {lat:32.560742, lng: -3.9314364},
-        zoom: 2
+        zoom: 2,
+        gestureHandling: 'greedy'
       });
 }
