ff9070f3d134da7f2e3fb3129eed9ef5f4a4b4e8
zurmuhlc <zurmuhlc@lafayette.edu>
2020-03-15 02:27:46 -0400

    set up the forms for the front-end

--- soccersite/map/static/map/js/selectors.js
+++ soccersite/map/static/map/js/selectors.js
@@ -1,19 +1,31 @@
 tail.select("#collegeLeagueSelector", {
   search: true,
   multiSelectAll: true,
-  placeholder: "College League"
+  placeholder: "College League",
+  sortItems: true
 });
 
 tail.select("#collegeSelector", {
   search: true,
   multiSelectAll: true,
-  placeholder: "College"
+  placeholder: "College",
+  sortItems: true
+});
+
+tail.select("#positionSelector", {
+  search: true,
+  multiSelectAll: true,
+  placeholder: "Position",
+  sortItems: true
 });
 
 tail.select("#starterSelector", {
-  placeholder: "Number of Years Starter"
+  placeholder: "Number of Years Starter",
+  multiSelectAll: true
 });
 
 tail.select("#allConferenceSelector", {
-  placeholder: "Number of Years All-Conference"
+  placeholder: "Number of Years All-Conference",
+  multiSelectAll: true,
+  width: 330
 });
