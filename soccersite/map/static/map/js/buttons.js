cc82017e90ca1927f20a308ecee514e4c6dc71de
zurmuhlc <zurmuhlc@lafayette.edu>
2020-03-15 12:30:59 -0400

    added more functionality to front end

---
+++ soccersite/map/static/map/js/buttons.js
@@ -0,0 +1,7 @@
+function submit() {
+  location.href = "#map"
+}
+
+function csv(){
+  //TODO: flush out csv function to save table to a csv file
+}
--- soccersite/map/static/map/js/map.js
+++ soccersite/map/static/map/js/map.js
@@ -1,7 +1,7 @@
 function initMap() {
   var map = new google.maps.Map(
       document.getElementById('map'), {
-        center: {lat:32.560742, lng: -3.9314364},
+        center: {lat: 32.560742, lng: -3.9314364},
         zoom: 2,
         gestureHandling: 'greedy'
       });
