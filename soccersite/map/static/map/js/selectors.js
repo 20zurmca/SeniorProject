7a7607faad7a965b644a6e0ab1f288a73762df2a
zurmuhlc <zurmuhlc@lafayette.edu>
2020-03-15 01:32:24 -0400

    added tail select for drop downs

---
+++ soccersite/map/static/map/js/selectors.js
@@ -0,0 +1,19 @@
+tail.select("#collegeLeagueSelector", {
+  search: true,
+  multiSelectAll: true,
+  placeholder: "College League"
+});
+
+tail.select("#collegeSelector", {
+  search: true,
+  multiSelectAll: true,
+  placeholder: "College"
+});
+
+tail.select("#starterSelector", {
+  placeholder: "Number of Years Starter"
+});
+
+tail.select("#allConferenceSelector", {
+  placeholder: "Number of Years All-Conference"
+});
