2643e8d4fc4e2ed736fd05be3f5126acf152db19
zurmuhlc <zurmuhlc@lafayette.edu>
2020-03-15 17:50:03 -0400

    finished UI mockup

--- soccersite/map/static/map/js/buttons.js
+++ soccersite/map/static/map/js/buttons.js
@@ -1,11 +1,22 @@
 function submit() {
-  location.href = "#map"
+  location.href = "#map";
+}
+
+
+function admin(){
+  window.open('http://127.0.0.1:8000/admin/', '_blank');
 }
 
 function csv(){
   //TODO: flush out csv function to save table to a csv file
 }
 
-function admin(){
-  location.href = "http://127.0.0.1:8000/admin"
+function about(){
+  //TODO: implement about page
 }
+
+//add event listeners
+document.getElementById("button1").addEventListener("click", submit, false);
+document.getElementById("button2").addEventListener("click", csv, false);
+document.getElementById("button3").addEventListener("click", admin, false);
+document.getelementById("button4").addEventListener("click", about, false);
