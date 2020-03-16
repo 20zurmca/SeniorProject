2a568fd95e0200889a66cdfdcb1e2a2849f3d404
zurmuhlc <zurmuhlc@lafayette.edu>
2020-03-16 14:58:23 -0400

    added AJAX call for form post

---
+++ soccersite/map/static/map/js/form.js
@@ -0,0 +1,39 @@
+/**
+ * getCookie() is used to get the csrftoken for ajax call
+*/
+function getCookie(c_name)
+{
+    if (document.cookie.length > 0)
+    {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1)
+        {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) c_end = document.cookie.length;
+            return unescape(document.cookie.substring(c_start,c_end));
+        }
+    }
+    return "";
+ }
+
+$(document).on('submit', '#filterForm', function(e){
+  e.preventDefault(); //prevent refresh
+  $.ajaxSetup({
+         headers: { "X-CSRFToken": getCookie("csrftoken") }
+     });
+  $.ajax({
+    type: 'POST',
+    url: '/',
+    data: { json_data: JSON.stringify({
+      collegeLeagues:$('#collegeLeagueSelector').val(),
+      colleges:$('#collegeSelector').val(),
+      positions:$('#positionSelector').val(),
+      starterYears:$('#starterSelector').val(),
+      allConferenceYears:$('#allConferenceYears').val(),
+    })},
+    success:function(){
+      console.log("Form submitted to server!")
+    }
+  });
+});
--- soccersite/map/views.py
+++ soccersite/map/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
 from .models import RosterMasterData
-from .forms import FilterForm
+import json
 
 # Create your views here.
 
@@ -11,17 +11,14 @@ def index(request):
     leagues   = RosterMasterData.objects.values_list('collegeLeague', flat=True).distinct().order_by('collegeLeague')
     positions = RosterMasterData.objects.values_list('position1', flat=True).distinct().order_by('position1')
 
-    if request.method == 'POST':
-        form  = FilterForm(request.POST)
-        if(form.is_valid()):
-            print(form.cleaned_data.get("collegeLeague"))
-    else:
-        form = FilterForm()
+    if(request.method == 'POST'):
+        payload = request.POST.get('json_data')
+        print(payload)
 
     context = {'API_KEY': settings.GOOGLE_MAPS_API_KEY,
                'colleges': colleges,
                'leagues': leagues,
-               'positions': positions,
-               'form': form }
+               'positions': positions
+               }
 
     return render(request, 'map/index.html', context)
