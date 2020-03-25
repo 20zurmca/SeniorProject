ab26099c8645cee382e24e20c0ec5fb7870963b1
Cameron Zurmuhl <zurmuhlc@lafayette.edu>
2020-03-25 17:27:37 -0400

    working on resizing the selectors

--- soccersite/map/static/map/js/selectors.js
+++ soccersite/map/static/map/js/selectors.js
@@ -39,7 +39,6 @@ var leagueSelector = tail.select("#collegeLeagueSelector", {
   search: true,
   multiSelectAll: true,
   placeholder: "College League",
-  width: 330
 });
 
 var collegeSelector = tail.select("#collegeSelector", {
@@ -63,7 +62,6 @@ var starterSelector = tail.select("#starterSelector", {
 var allConferenceSelector = tail.select("#allConferenceSelector", {
   placeholder: "Number of Years All-Conference",
   multiSelectAll: true,
-  width: 330,
   multiShowCount: false,
 });
 
