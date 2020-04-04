92d8c21bcec1d0ccca9de6fb2f4a03082908862b
weix <weix@lafayette.edu>
2020-04-04 10:38:29 -0400

    add upload csv and save the csv file to the server and update the database feature

--- soccersite/soccersite/urls.py
+++ soccersite/soccersite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('map.urls')),
+    path('admin/myadmin/', include('map.urls')),
     path('admin/', admin.site.urls),
 ]
