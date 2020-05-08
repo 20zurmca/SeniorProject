e2d62522106f04fc6f7e7cd523a098b913ef84f9
Cameron Zurmuhl <zurmuhlc@lafayette.edu>
2020-05-07 22:43:51 -0400

    pushing server changes

--- soccersite/static/map/js/form.js
+++ soccersite/static/map/js/form.js
@@ -1,4 +1,9 @@
 /**
+  form.js is for submitting the main filter form
+*/
+
+
+/**
  * getCookie() is used to get the csrftoken for ajax call
 */
 var dt;
@@ -19,60 +24,121 @@ function getCookie(c_name)
     return "";
  }
 
+/**
+ * _findLatestIndex(roster_year) returns the maped indices of ordered roster_years to the non-ordered
+ * roster_years list for a player
+ * @param roster_year the string of roster years from the db
+ * @return list: list position of the roster years from oldest to most recent
+*/
+ function _findAssociativeIndices(roster_year){
+   let association = []
+   let rosterYearArr = roster_year
+   let sortedYearArr = roster_year.sort();
+   let year = '';
+   for(var i = 0; i < roster_year.length; i++){
+     association.push(rosterYearArr.indexOf(sortedYearArr[i]));
+   }
+   return association; //max year's index is at the last index of association array
+ }
+
+ /**
+  * _sortAggregateData sorts an array aggregate type from our db
+  * @param data the ArrayAggregate
+  */
+ function _sortAggregateData(data){
+   return data.sort();
+ }
+
+ /**
+  * _getCurrentDataElement(data, currentIndex) gets most current entry of an
+  * an array aggregate type by using the currentIndex acquired from _findLatestIndex(roster_year).
+  * If the most current entry is null, then return the max element to display
+  * @param data the ArrayAggregate data
+  * @param association the index association arrray of the roster_year
+ */
+ function _getCurrentDataElement(data, association){
+   let d = data;
+   if(d[association[association.length-1]]){ //if there is an entry correlating to max roster_year
+     return d[association[association.length-1]];
+   } else {
+     for(var i = association.length - 2; i >= 0; i--){ //find most recent association
+       if(d[association[i]]){
+         return d[association[i]];
+       }
+     }
+   }
+   return ''; //all null in the data
+ }
+
+ function convertToInches(height){
+   if(height){
+     data = height.split("'");
+     return 12 * parseInt(data[0]) + parseInt(data[1]);
+   }
+   return '';
+ }
+
+ function goToRosterPage(page){
+   window.open(page, "_blank");
+ }
+
 $(document).on('submit', '#filterForm', function(e){
   e.preventDefault(); //prevent refresh
-  selectedColleges           = collegeSelector.options.find("selected", "any"); //from selectors.js
-  selectedPositions          = positionSelector.options.find("selected", "any");
-  selectedStarterYears       = starterSelector.options.find("selected", "any");
-  selectedAllConferenceYears =  allConferenceSelector.options.find("selected", "any");
-  let selectedSomething = (selectedColleges.length > 0 || selectedPositions.length > 0 ||
-                          selectedStarterYears.length > 0 || selectedAllConferenceYears.length > 0);
-  if(!selectedSomething){
-    alert("Select at least one drop down to query.");
-  } else {
-    $.ajaxSetup({
-           headers: { "X-CSRFToken": getCookie("csrftoken") }
-       });
-    $.ajax({
-      type: 'POST',
-      url: window.location,
-      data: { json_data: JSON.stringify({
-        collegeLeagues:$('#collegeLeagueSelector').val(),
-        colleges:$('#collegeSelector').val(),
-        positions:$('#positionSelector').val(),
-        starterYears:$('#starterSelector').val(),
-        allConferenceYears:$('#allConferenceSelector').val()
-      })},
-      success:function(response){
-        loadData(map, response['players']); //loading data in map.js
-        var player_data = '';
-        if(dt){
-          dt.destroy();
-        }
-        $.each(response['players'], function(key, value){
-          player_data += '<tr>';
-          player_data += '<td>' + value.rosterYear     + '</td>';
-          player_data += '<td>' + value.firstName      + '</td>';
-          player_data += '<td>' + value.lastName       + '</td>';
-          player_data += '<td>' + value.year           + '</td>';
-          player_data += '<td>' + value.position1      + '</td>';
-          player_data += '<td>' + "will implement"     + '</td>';
-          player_data += '<td>' + "will implement"     + '</td>';
-          player_data += '<td>' + value.collegeLeague  + '</td>';
-          player_data += '<td>' + value.college        + '</td>';
-          player_data += '<td>' + value.homeTown       + '</td>';
-          player_data += '<td>' + value.stateOrCountry + '</td>';
-          player_data += '<td>' + value.highSchool     + '</td>';
-          player_data += '</tr>';
-        });
-        document.getElementById('resultTableBody').innerHTML = player_data;
-        dt = $('#resultTable').DataTable({
-          "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
-        });
-      },
-      error:function(){
-        console.log("Error with form submission");
-      }
-    });
+
+  let multipleChecked = 'selected';
+  if (!$("#moreThanOneSchool").is(":checked")) {
+    multipleChecked = '';
   }
+
+  $.ajaxSetup({
+          headers: { "X-CSRFToken": getCookie("csrftoken") }
+      });
+
+  $.ajax({
+    type: 'POST',
+    url: window.location,
+    data: { json_data: JSON.stringify({
+      collegeLeagues:$('#collegeLeagueSelector').val(),
+      colleges:$('#collegeSelector').val(),
+      positions:$('#positionSelector').val(),
+      starterYears:$('#starterSelector').val(),
+      allConferenceYears:$('#allConferenceSelector').val(),
+      multipleHS:multipleChecked
+    })},
+    success:function(response){
+      loadData(map, response['players']); //loading data in map.js
+      var player_data = '';
+      if(dt){
+        dt.destroy();
+      }
+
+      $.each(response['players'], function(key, value){
+        let association = _findAssociativeIndices(value.roster_year);
+        let currentBioLink = _getCurrentDataElement(value.bio_link, association);
+        player_data += '<tr onclick = goToRosterPage("'.concat(currentBioLink).concat('")>');
+        player_data += '<td>' + _sortAggregateData(value.roster_year)                                + '</td>';
+        player_data += '<td>' + value.first_name                                                     + '</td>';
+        player_data += '<td>' + value.last_name                                                      + '</td>';
+        player_data += '<td>' + _getCurrentDataElement(value.position, association)                  + '</td>';
+        player_data += '<td>' + convertToInches(_getCurrentDataElement(value.heights, association))  + '</td>';
+        player_data += '<td>' + _getCurrentDataElement(value.weights, association)                   + '</td>';
+        player_data += '<td>' + value.starter_count                                                  + '</td>';
+        player_data += '<td>' + value.accolade_count                                                 + '</td>';
+        player_data += '<td>' + value.college_league                                                 + '</td>';
+        player_data += '<td>' + value.college                                                        + '</td>';
+        player_data += '<td>' + value.home_town                                                      + '</td>';
+        player_data += '<td>' + value.state_or_country                                               + '</td>';
+        player_data += '<td>' + value.high_school                                                    + '</td>';
+        player_data += '</tr>';
+      });
+      document.getElementById('resultTableBody').innerHTML = player_data;
+      dt = $('#resultTable').DataTable({
+        "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+        "scrollX": true
+      });
+    },
+    error:function(){
+      console.log("Error with form submission");
+    }
+  });
 });
--- soccersite/static/map/js/homePageButtons.js
+++ soccersite/static/map/js/homePageButtons.js
@@ -1,34 +1,28 @@
+/*
+ homePagebuttons.js provides client-side logic for interacting with buttons on
+ the homepage
+*/
+
+//blue submit button
 function submit() {
-  selectedColleges           = collegeSelector.options.find("selected", "any"); //from selectors.js
-  selectedPositions          = positionSelector.options.find("selected", "any");
-  selectedStarterYears       = starterSelector.options.find("selected", "any");
-  selectedAllConferenceYears =  allConferenceSelector.options.find("selected", "any");
-  let selectedSomething      = (selectedColleges.length > 0 || selectedPositions.length > 0 ||
-                                selectedStarterYears.length > 0 || selectedAllConferenceYears.length > 0);
-  if(selectedSomething){
-      document.getElementById('markerControl').style.fontWeight = "bold";
-      document.getElementById('markerControl').disabled = false;
-      document.getElementById('zoomControl').disabled = false;
-      document.getElementById('heatMapControl').disabled = false;
-      if(document.getElementById('noQuerySelected')){
-        document.getElementById('noQuerySelected').style.display = "none";
-      }
-      submitted=true;
-      $('.loader').show();
-    }
+  document.getElementById('markerControl').style.fontWeight = "bold";
+  document.getElementById('markerControl').disabled = false;
+  document.getElementById('zoomControl').disabled = false;
+  document.getElementById('heatMapControl').disabled = false;
+  if(document.getElementById('noQuerySelected')){
+      document.getElementById('noQuerySelected').style.display = "none";
+  }
+  submitted=true; //declaration found on line 127 in index.html
+  $('.loader').show();
 }
 
-
-function admin(){
-  window.open('/admin/');
-}
-
+//helper function for csv download
 function parseCell(tableCell){
   var parsedValue = tableCell.textContent;
   parsedValue = parsedValue.replace(/"/g, '""'); //replace all single quote with double quote
 
   //if value contains comma, new-line or double-quote, enclose in double quotes
-  parsedValue = /[",\n]/.test(parsedValue) ? '"${parsedValue}"' : parsedValue;
+  parsedValue = /[",\n]/.test(parsedValue) ? parsedValue.split(',').join(' ') : parsedValue;
 
   return parsedValue;
 }
@@ -71,13 +65,6 @@ function csv(){
 
 }
 
-function about(){
-  window.location.href = '/about'
-}
-
-
 //add event listeners
 document.getElementById("button1").addEventListener("click", submit, false);
 document.getElementById("button2").addEventListener("click", csv, false);
-// document.getElementById("button3").addEventListener("click", admin, false);
-// document.getElementById("button4").addEventListener("click", about, false);
